52c35a119e72ea588762ac246a94f80409d23154
David69699 <abutidzedato18@gmail.com>
2023-10-27 15:54:42 +0400

    davaleba13

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
 })
+
+console.log("Task N1")
+for (let i = 1; i <= 10; i++) {
+    console.log(3 * i);
+}
+console.log("Task N2")
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+console.log("Task N3")
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+  sum += i;
+}
+
+console.log(""+ sum)
