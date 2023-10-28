40c7c25f2e7e054b1250ef0518ce8df7556ce7d7
David69699 <abutidzedato18@gmail.com>
2023-10-28 14:22:56 +0400

    davaleba13

--- main.js
+++ main.js
@@ -23,3 +23,19 @@ for (let i = 1; i <= 100; i++) {
 }
 
 console.log(""+ sum)
+
+console.log("Task N4")
+
+let num = 16
+
+for(let i = 1; i <= 16; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    }else if(i % 3 === 0){
+        console.log("Fizz")
+    }else if(i % 5 === 0){
+        console.log("Buzz")
+    }else{
+        console.log(i)
+    }
+}
